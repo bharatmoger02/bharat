74a7a5d09cbccacf5ac8dea6553fc1830ce301a9
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-05 18:03:59 +0000

    var

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,6 @@
-const accId = 14
-let accEmail = "abc@gmail.com"
-let accName
-console.table([accId,accEmail,accName])
-console.log(typeof accName)
+//const accId = 14
+//let accEmail = "abc@gmail.com"
+//let accName
+//console.table([accId,accEmail,accName])
+//console.log(typeof accName)
+console.log(typeof null)
