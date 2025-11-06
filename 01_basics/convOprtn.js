5857e28bf48d7e30ee0690e17085ece22878d325
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-06 18:25:13 +0000

    learnt about comparisons and datatypes

---
+++ 01_basics/convOprtn.js
@@ -0,0 +1,22 @@
+/*let a = 23
+let b = -a
+console.log(b)*/
+/*let str1 = "bharat"
+let str2 = " moger"
+let str3 = str1+str2
+console.log(str3);*/
+//console.log(+true);
+//let v = 20;
+//console.log(v);
+let x = 3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+/*const id = Symbol('123')
+const aid = Symbol('123')
+console.log(id)
+console.log(aid);*/
+//e = null
+//console.log(e);
+//console.log(typeof e);
+
+//const cricketers = ["virat", "msd", "rohit"];
+//console.log(cricketers);
+/*let myObj = {
+    name : "bharat",
+    age : 21,
+};
+console.log(myObj);*/
+const myFunction = function(){
+    console.log("hello world");
+}
+myFunction();
+
