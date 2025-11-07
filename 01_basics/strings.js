d195230f3bf647dcae0c62f32e289f25404f3e9b
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-07 19:07:52 +0000

    strings

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,10 +1,15 @@
-const fullName = new String ('bharatrm')
+//const fullName = new String ('bharatrm')
 /*console.log(fullName[4]);
 console.log(fullName.length);
 console.log(fullName.toUpperCase());*/
 //const nameSlice = fullName.substring(0,4)
 //console.log(nameSlice);
-const anoSlice = fullName.slice(-8,4)
-console.log(anoSlice);
-
-
+//const anoSlice = fullName.slice(-8,4)
+//console.log(anoSlice);
+//const url = "https://bharat.com/bharat%20%moger"
+//console.log(url.replace('%20%','-'));
+//console.log(url.includes('moger'));
+//console.log(fullName.split('-'));
+let name = "bharat"
+let age = 21
+console.log(`hello, my name is ${name} and my age is ${age}`);
