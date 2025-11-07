99c82dd39d5c7b5309a60f4cf2abb40b264d661d
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-07 17:47:10 +0000

    str

---
+++ 01_basics/strings.js
@@ -0,0 +1,10 @@
+const fullName = new String ('bharatrm')
+/*console.log(fullName[4]);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());*/
+//const nameSlice = fullName.substring(0,4)
+//console.log(nameSlice);
+const anoSlice = fullName.slice(-8,4)
+console.log(anoSlice);
+
+
