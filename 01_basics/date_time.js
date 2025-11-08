8f407c2aaa8c51ab6e10b5a711b48577f305fb8e
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-08 18:29:26 +0000

    date and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,7 @@
+//let myDate = new Date()
+//console.log(myDate.toLocaleTimeString());
+let myCreatedDate = new Date("07-02-2004")
+console.log(myCreatedDate.toDateString());
+//let myTimeStamp = Date.now()
+console.log(myCreatedDate.getTime());
+
