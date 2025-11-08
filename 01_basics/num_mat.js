a8bc709c406e679638fa2ffd5747660c79b6c7f2
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-08 06:33:25 +0000

    num's and maths

---
+++ 01_basics/num_mat.js
@@ -0,0 +1,14 @@
+/*const balance = new Number (100)
+console.log(balance);
+console.log(balance.toFixed(3));
+console.log(balance.toString());*/
+//const num1 = new Number(2352144)
+//console.log(num1.toPrecision(2));
+//console.log(num1.toLocaleString('en-IN'));
+//console.log(Math.random()*10);
+const min = 10
+const max = 20
+console.log(Math.floor (Math.random()*10) + min);
+
+
+
