f5eb63940b7c8df1ef2c372b531f214503609a7f
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-09 17:08:53 +0000

    arrays

---
+++ 01_basics/arrays.js
@@ -0,0 +1,20 @@
+//const myArr = [1,2,3,4,5]
+//console.log(myArr);
+//const myArr2 = new Array(1,2,3,4)
+//myArr.push(4)
+//myArr.unshift(1)
+//console.log(myArr.includes(9));
+//const newArr2 = myArr.splice(1,3)
+//const newArr = myArr.slice(1,3)
+//console.log(newArr2);
+//console.log(myArr);
+//const ind_cric = ["virat", "rohit", "jassi"]
+//const aus_cric = ["steve", "travis", "pat"]
+//ind_cric.push(aus_cric)
+//console.log(ind_cric);
+//***spread***//
+//const all_new_cric = [...ind_cric, ...aus_cric] 
+// //const all_cric = ind_cric.concat(aus_cric)
+//console.log(all_new_cric);
+console.log(Array.isArray("bharat"));
+console.log(Array.from("bharat"));
