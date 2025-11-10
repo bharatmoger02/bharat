2058044ade7697143d0911ca747b2b275df86bb3
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-10 16:42:01 +0000

    functions

---
+++ 01_basics/func.js
@@ -0,0 +1,19 @@
+/*function addTwoNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addTwoNumbers(3,7)*/
+/*function add (n1,n2){
+    let res = n1 + n2
+    return res
+}
+res = add (3,5)
+console.log("result:", res);*/
+/*function logIn(username="bharat"){
+    if(username === undefined){
+        console.log("pls enter username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(logIn());*/
+
