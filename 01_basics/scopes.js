fcf542060f5a161e922ef844d5c6c941b021e306
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-12 12:54:21 +0000

    scopes

---
+++ 01_basics/scopes.js
@@ -0,0 +1,23 @@
+/*let a = 300
+if (true){
+   let a=10
+   console.log("inner:", a);
+   
+}
+console.log(a);*/
+//************nested loops************* *//
+/*function one(){
+    const username = "bharat"
+    function two(){
+        let website = "instagram"
+        console.log(username)
+        console.log(website);
+        
+    }
+    two()
+}
+one()*/
+const addOne = function(num){
+    return num + 4
+}
+console.log(addOne(6))
