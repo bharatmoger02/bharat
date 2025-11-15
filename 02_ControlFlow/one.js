006bbd6961e201d90125acc04c3116145198f97c
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-15 05:52:16 +0000

    if else

---
+++ 02_ControlFlow/one.js
@@ -0,0 +1,18 @@
+/*if(2==4){
+    console.log("executes");
+} else{
+    console.log("nope");
+}*/
+//if (50>100) console.log("50 greater no."); else console.log("50 lesser No.");
+const Bharat = true
+const Lohit = true
+const Siblings = true 
+if(Bharat && Siblings && Lohit){
+    console.log("they are siblings");
+    
+}else{
+    console.log("they aren't siblings");
+    
+}
+
+
