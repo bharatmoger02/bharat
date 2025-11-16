f067b614e573e6a1e78b57542df7c26e2f080570
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-16 05:06:56 +0000

    ctrl_flow

---
+++ 02_ControlFlow/switch.js
@@ -0,0 +1,20 @@
+const month = "mar"
+
+switch (month){
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("none");
+        break;
+}
---
+++ 02_ControlFlow/truth_false.js
@@ -0,0 +1,11 @@
+/*const userEmail =function(){}
+if(userEmail){
+    console.log("i've got email");
+    
+}else{
+    console.log("did'nt recieve email");
+    
+}*/
+const BiscuitPrize = 10
+const myMoney = 5
+myMoney >= BiscuitPrize ? console.log("give me that biscuit") : console.log("ye 10 ruppay ke biscuit kitne ka h ji?");
