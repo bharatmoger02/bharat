697d35aba07ecc0ce785beeeb1d002e70e83755f
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-16 06:15:45 +0000

    for loop

---
+++ 02_ControlFlow/for.js
@@ -0,0 +1,19 @@
+/*for (let i = 10; i <= 20; i++) {
+    console.log(`tables of value : ${i}`);
+    for (let j=1; j<=10; j++){
+    console.log(i +'*'+ j + '=' + i*j);
+    }
+}*/
+/*const myArray = ["virat", "rohit", "msd"]
+for(let index=0; index<myArray.length; index++){
+    console.log(myArray[index]);
+    
+}*/
+
+for(let i=0; i<=20 ; i++){
+    if(i==10){
+        console.log(`middle no.detected i.e., ${i}`);
+        continue
+    }
+        console.log(`val of i is ${i} `);
+}
