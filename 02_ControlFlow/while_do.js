3bda2d40102cc60a7d3a12244d250731c79f9449
bharatmoger02 <03bharatmgr@gmail.com>
2025-11-17 16:28:18 +0000

    do and while

---
+++ 02_ControlFlow/while_do.js
@@ -0,0 +1,13 @@
+/*const myArray = ["salah","vanDijk","alison"]
+let arr=0
+while(arr<myArray.length){
+    console.log(myArray[arr]);
+    arr=arr+1
+}*/
+
+//************************do_while****************************
+let i=1
+do{
+    console.log(i);
+    i++
+} while(i<10)
