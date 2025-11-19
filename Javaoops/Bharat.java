8665232b4dfb98d6ec2f779106ee796f21158145
Bharat Moger <03bharatmgr@gmail.com>
2025-11-19 16:52:26 +0000

    classes and public classes

---
+++ Javaoops/Bharat.java
@@ -0,0 +1,24 @@
+class A{
+    public static void main(String args[]){
+        System.out.println("abc");
+    }
+
+}
+class B{
+    public static void main(String args[]){
+        System.out.println("jhk");
+    }
+
+}
+class C{
+    public static void main(String args[]){
+        System.out.println("ghi");
+    }
+
+}
+class D{
+    public static void main(String args[]){
+        System.out.println("def");
+    }
+
+}
