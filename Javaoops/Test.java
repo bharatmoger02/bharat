45028f0af20ea6f67ad8bbfeef0bd9691a53db42
Bharat Moger <03bharatmgr@gmail.com>
2025-11-19 17:55:37 +0000

    import statement

---
+++ Javaoops/Test.java
@@ -0,0 +1,9 @@
+//import java.util.ArrayList; ******expicit*********
+import java.util.*; //implicit
+public class Test
+{
+    public static void main(String[] args)
+    {
+        ArrayList l = new ArrayList();
+    }
+}
