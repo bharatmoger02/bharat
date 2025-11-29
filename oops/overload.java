2769d4cccc1d5ee80da939129ee985cc29468536
Bharat Moger <03bharatmgr@gmail.com>
2025-11-29 07:00:15 +0000

    cases in overloading

--- oops/overload.java
+++ oops/overload.java
@@ -1,17 +1,37 @@
-class overload {
+/*class overload {
     public void m1(){
         System.out.println("no-argument");
     }
     public void m1(int i){
         System.out.println("int-arg");
     }
-    public void m1(double d){
-        System.out.println("double-arg");
+    public void m1(float f){
+        System.out.println("float-arg");
     }
     public static void main(String args[]){
         overload o = new overload();
         o.m1();
         o.m1(10);
-        o.m1(20.5);
+        o.m1(20.5f);
+        o.m1('b');
+        o.m1(10l);
+        //o.m1(10.5);
+    }
+}*/
+
+class overload {
+    public void m1(Object o){
+        System.out.println("object version");
+    }
+    public void m1(String s){
+        System.out.println("string version");
+    }
+
+    public static void main(String[] args)
+    {
+     overload o = new overload();
+        o.m1(new Object());
+        o.m1("string");
+        o.m1(null);
     }
 }
