b2eb5463f705794396373df1066251306cdf9717
Bharat Moger <03bharatmgr@gmail.com>
2026-01-10 03:50:05 +0000

    .

---
+++ prac/abc.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class abc
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("BMW");
+        cars.add("ford");
+        cars.add("mclaren");
+        cars.add("mitsbuishi");
+        System.out.println(cars);
+    }
+}
